d2f3c84f887fffe03ae158c39cfa83e164e0e4ea
Larry Burris III <larry.n.burris@gmail.com>
2017-04-23 01:26:07 -0400

    Add config.json for storing environment config variables
    Add dummy variables that can be overwritten by environment paramaters

--- src/LL.NET.Blog.Web/Startup.cs
+++ src/LL.NET.Blog.Web/Startup.cs
@@ -7,25 +7,25 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using LL.NET.Blog.Data.Repositories;
 
 namespace LL.NET.Blog.Web
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
         public Startup(IHostingEnvironment env)
         {
+            _env = env;
+
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("config.json", false, true)
                 .AddEnvironmentVariables();
 
-            if (env.IsDevelopment())
-            {
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-                builder.AddApplicationInsightsSettings(developerMode: true);
-            }
-            Configuration = builder.Build();
+            _config = builder.Build();
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -33,32 +33,32 @@ namespace LL.NET.Blog.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddApplicationInsightsTelemetry(Configuration);
-
+            if (_config["BlogDb:TestData"] == "True")
+            {
+                services.AddScoped<IBlogRepository, MemoryRepository>();
+            }
+            else
+            {
+                //services.AddScoped<IBlogRepository, DbRepository>();
+            }
+            
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            app.UseApplicationInsightsRequestTelemetry();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
+                loggerFactory.AddDebug();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
-            app.UseApplicationInsightsExceptionTelemetry();
-
+            
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
