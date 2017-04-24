38fe33d2a9fbbbb9076a2503f775d3c301dfd583
Larry Burris III <larry.n.burris@gmail.com>
2017-04-24 02:02:58 -0400

    Refactor home page and home controller

--- src/LL.NET.Blog.Web/Controllers/HomeController.cs
+++ src/LL.NET.Blog.Web/Controllers/HomeController.cs
@@ -50,20 +50,6 @@ namespace LL.NET.Blog.Web.Controllers
             return Redirect("/");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
