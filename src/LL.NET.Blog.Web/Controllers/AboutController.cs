409ecbd028b82f35e867ee603365df44e72a6d38
Larry Burris III <larry.n.burris@gmail.com>
2017-04-24 01:59:28 -0400

    Add controller and view for about page

---
+++ src/LL.NET.Blog.Web/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LL.NET.Blog.Web.Controllers
+{
+    [Route("about")]
+    public class AboutController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            ViewBag.PageHeaderTitle = "Larry Burris";
+            ViewBag.PageHeaderSubtitle = "A Student and a Teacher";
+            return View();
+        }
+    }
+}
